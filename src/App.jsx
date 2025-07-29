12f69ab62f32a9ba5fb4a4518f24c85eaee29cca
Gauri-Moghe <gauri.s.moghe@gmail.com>
2025-07-29 14:18:24 -0700

    Configuring the project for the Github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
+// import './css/App.css'
+// import Favorites from './pages/Favorites';
+// import Home from './pages/Home';
+// import {Routes, Route} from "react-router-dom"
+// import { MovieProvider } from './contexts/MovieContext';
+// import NavBar from './components/NavBar';
+
+// function App() {
+
+//   return (
+//     <MovieProvider>
+//       <NavBar></NavBar>
+//       <main className="main-content">
+//         <Routes>
+//           {/* <Route path='/' element = {<Home />}/>  
+//           <Route path='/favorites' element = {<Favorites />}/>   */}
+
+//           <Route path="/" element={<Home key="home" />} />  
+//           <Route path="/favorites" element={<Favorites />} />  
+
+        
+//         </Routes>
+//       </main>
+//     </MovieProvider>
+//   );
+// }
+
+
+// export default App
+
+
 import './css/App.css'
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
-import {Routes, Route} from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { MovieProvider } from './contexts/MovieContext';
 import NavBar from './components/NavBar';
 
 function App() {
-
   return (
     <MovieProvider>
-      <NavBar></NavBar>
-      <main className="main-content">
-        <Routes>
-          {/* <Route path='/' element = {<Home />}/>  
-          <Route path='/favorites' element = {<Favorites />}/>   */}
-
-          <Route path="/" element={<Home key="home" />} />  
-          <Route path="/favorites" element={<Favorites />} />  
-
-        
-        </Routes>
-      </main>
+      <Router>
+        <NavBar></NavBar>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home key="home" />} />  
+            <Route path="/favorites" element={<Favorites />} />  
+          </Routes>
+        </main>
+      </Router>
     </MovieProvider>
   );
 }
 
-
-export default App
+export default App;
